83e34dba68c8b9fd8eb94fd2313ea852e5b907e0
Hanwei Wang <wanghanwei7@163.com>
2025-05-27 18:17:38 -0700

    Change the title and description in config.ts

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -8,13 +8,13 @@ export default defineUserConfig({
   locales: {
     "/": {
       lang: "en-US",
-      title: "Docs Demo",
-      description: "A docs demo for vuepress-theme-hope",
+      title: "SuperWhw",
+      description: "SuperWhw's personal website",
     },
     "/zh/": {
       lang: "zh-CN",
-      title: "文档演示",
-      description: "vuepress-theme-hope 的文档演示",
+      title: "SuperWhw",
+      description: "SuperWhw 的个人网站",
     },
   },
 
